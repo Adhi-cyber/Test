d55bb3c7eb44bf28e934335d3b6bcc2ba9680e32
Adhi-cyber <112969103+Adhi-cyber@users.noreply.github.com>
2023-02-27 09:51:52 +0530

    Delete 2 directory

--- 2/buffer_ass.v
+++
@@ -1,8 +0,0 @@
-module buffer_ass(
-	input d,
-	input e,
-	output y);
-
-assign y = e ? d : 1'b0;
-
-endmodule
--- 2/buffer_gate.v
+++
@@ -1,8 +0,0 @@
-module buffer_gate(
-	input d,
-	input e,
-	output y);
-
-and(y,d,e);
-
-endmodule
